bf1fd80cd08aa6885e88f91299357f52d0ae65ea
Rute Santos <rute_santos@harvard.edu>
2018-03-01 11:54:19 -0500

    Changed operation and tests to use attachments for dfxp. Updated documentation.

--- modules/transcription-service-workflowoperation/src/main/java/org/opencastproject/transcription/workflowoperation/AttachTranscriptionOperationHandler.java
+++ modules/transcription-service-workflowoperation/src/main/java/org/opencastproject/transcription/workflowoperation/AttachTranscriptionOperationHandler.java
@@ -108,11 +108,15 @@ public class AttachTranscriptionOperationHandler extends AbstractWorkflowOperati
     // Check which tags/flavors have been configured
     String targetTagOption = StringUtils.trimToNull(operation.getConfiguration(TARGET_TAG));
     String targetFlavorOption = StringUtils.trimToNull(operation.getConfiguration(TARGET_FLAVOR));
-    // Target flavor is mandatory
-    if (targetFlavorOption == null)
-      throw new WorkflowOperationException(TARGET_FLAVOR + " missing");
-    MediaPackageElementFlavor flavor = MediaPackageElementFlavor.parseFlavor(targetFlavorOption);
     String captionFormatOption = StringUtils.trimToNull(operation.getConfiguration(TARGET_CAPTION_FORMAT));
+    // Target flavor is mandatory if target-caption-format was NOT informed and no conversion is done
+    if (targetFlavorOption == null && captionFormatOption == null)
+      throw new WorkflowOperationException(TARGET_FLAVOR + " missing");
+    // Target flavor is optional if target-caption-format was informed because the default flavor
+    // will be "captions/<format>". If informed, will override the default.
+    MediaPackageElementFlavor flavor = null;
+    if (targetFlavorOption != null)
+      flavor = MediaPackageElementFlavor.parseFlavor(targetFlavorOption);
 
     try {
       // Get transcription file from the service
@@ -128,8 +132,9 @@ public class AttachTranscriptionOperationHandler extends AbstractWorkflowOperati
         transcription = MediaPackageElementParser.getFromXml(job.getPayload());
       }
 
-      // Set the target flavor
-      transcription.setFlavor(flavor);
+      // Set the target flavor if informed
+      if (flavor != null)
+        transcription.setFlavor(flavor);
 
       // Add tags
       if (targetTagOption != null) {
